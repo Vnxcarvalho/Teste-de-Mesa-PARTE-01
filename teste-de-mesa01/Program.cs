866e386bce35253031d59638c2beccabd90e04f5
Alan Carvalho <alanviniciuscarvalho@gmail.com>
2026-06-23 09:28:46 -0300

    first commit

---
+++ teste-de-mesa01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+
+        double c = (a + b) / 2.0;
+        c = c - 40;
+
+        double[] v = new double[4];
+
+        v[3] = a + b + c;
+
+        Console.WriteLine("A = " + a);
+        Console.WriteLine("B = " + b);
+        Console.WriteLine("C = " + c);
+        Console.WriteLine("V[3] = " + v[3]);
+    }
+}
---
+++ teste-de-mesa03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int a = 7;
+            int b = a - 6;
+
+        int[] v = new int[7];
+
+        while (b < a)
+        {
+            v[b] = b + a;
+            b = b + 2;
+        }
+
+        for (int i = 0; i < v.Length; i++)
+        {
+            Console.WriteLine($"v[{i}] = {v[i]}");
+        }
+    }
+}
+
